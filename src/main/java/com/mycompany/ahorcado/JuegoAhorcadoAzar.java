5133acb86d97a1ba09e478bdf4f707737d4998d4
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-08-11 16:09:24 -0600

    azar

---
+++ src/main/java/com/mycompany/ahorcado/JuegoAhorcadoAzar.java
@@ -0,0 +1,76 @@
+
+package com.mycompany.ahorcado;
+//Nicole
+
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class JuegoAhorcadoAzar extends JuegoAhorcadoBase{
+      
+      
+      
+      
+    public JuegoAhorcadoAzar(ArrayList<String> palabrasPosibles) {
+        this.ListaPalabras = palabrasPosibles;
+        
+        
+    }
+    
+    @Override
+    void actualizarPalabraActual(char letra) {//Actualiza la palabra actualizada a medida que el jugador adivina letras.
+        StringBuilder nuevaPalabraActual = new StringBuilder(palabraActual);
+        
+          for (int i = 0; i < palabraSecreta.length(); i++) {
+              if(verificarLetra(letra)){
+              nuevaPalabraActual.setCharAt(i, letra);
+              Jlabel.setText(nuevaPalabraActual);
+          } 
+        }
+          if(!verificarLetra(letra)){
+            JOptionPane.showMessageDialog(null, "No se encuentra esa letra en la palabra");
+          }
+    }
+        
+
+    @Override
+    boolean verificarLetra(char letra) {// Verifica si la letra ingresada por el jugador es correcta.
+        
+        return palabraSecreta.contains(String.valueOf(letra));
+    }
+
+    @Override
+    boolean hasGanado() {//Verifica si el jugador ha ganado el juego
+          return palabraActual.equals(palabraSecreta);
+        
+        }
+
+    @Override
+    public void inicializarPalabraSecreta() {//incializa la palabra a usar
+        AdminPalabrasSecretas admin =new  AdminPalabrasSecretas();
+        admin.seleccionarPalabraAlAzar();
+        
+         }
+
+    @Override
+    public void jugar() {
+        inicializarPalabraSecreta();
+        inicializarPalabraSecreta();
+
+        while (intentos > 0 && !hasGanado()) {
+            char letraIngresada = Jlabel.getText();
+            if (verificarLetra(letraIngresada)) {
+                actualizarPalabraActual(letraIngresada);
+            } else {
+                intentos--;
+            }
+        }
+        if(hasGanado()){
+            JOptionPane.showMessageDialog(null, "¡HAS GANADO, FELICIDADES!");
+        }else{
+            JOptionPane.showMessageDialog(null, "Has perdido, se acabaron los intentos");
+        }
+        }
+    
+}
