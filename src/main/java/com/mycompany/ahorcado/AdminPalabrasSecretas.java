fbaeeb1f05665d1a71e87d6603ad1ff7d0206811
josuc <josuc@Josue>
2023-08-11 14:39:04 -0600

    clase AdminPalabrasSecretas

---
+++ src/main/java/com/mycompany/ahorcado/AdminPalabrasSecretas.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ahorcado;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author josuc
+ */
+public class AdminPalabrasSecretas {
+    private ArrayList <String> palabrasSecretas;
+    
+    public AdminPalabrasSecretas () {
+        this.palabrasSecretas = new ArrayList<>();
+    }
+    
+    public void agregarPalabra(String palabra) {
+        this.palabrasSecretas.add(palabra);
+    }
+    
+    public String seleccionarPalabraAlAzar() {
+        Random random = new Random();
+        int posicionRandom = random.nextInt(0, palabrasSecretas.size());
+        return palabrasSecretas.get(posicionRandom);
+    }
+}
